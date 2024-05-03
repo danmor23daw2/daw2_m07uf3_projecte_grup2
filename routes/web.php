1e584f1e23ebbf0180265d3be43069f6f63e4c73
danmor23daw2 <15586175.clot@fje.edu>
2024-05-03 14:04:55 +0000

    Commit 1 del projecte RentByRent

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,51 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorTreballador;
 
 Route::get('/', function () {
     return view('inici');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource('trebs', ControladorTreballador::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+use App\Models\Treballador;
+Route::post('/noureg', function () {
+$dades= array("nom"=>"Jaume", "cognoms"=>"Pons", "nif"=>"46108810Q",
+"data_naixement"=>"2000-03-15","sexe"=>"d",
+"adressa"=>"Carrer de Valènica, 690, 1-2", "tlf_fixe"=>934567890,
+"tlf_mobil"=>666339900, "email"=>"jpons@gmail.com",
+"treball_distancia"=>true,"tipus_contracte"=>"temporal",
+"data_contractacio"=>"2018-03-15","categoria"=>3,
+"nom_feina"=>"desenvolupador junior", "sou"=>1404,47);
+Treballador::create($dades);
+$dades= array("nom"=>"Anna", "cognoms"=>"Puig", "nif"=>"47108810P",
+"data_naixement"=>"2000-03-15","sexe"=>"d",
+"adressa"=>"Carrer de Valènica, 680, 2-2", "tlf_fixe"=>934567899,
+"tlf_mobil"=>666339911, "email"=>"apuig@gmail.com",
+"treball_distancia"=>false,"tipus_contracte"=>"temporal",
+"data_contractacio"=>"2021-03-15","categoria"=>2,
+"nom_feina"=>"desenvolupador senior", "sou"=>1704,97);
+Treballador::create($dades);
+});
+Route::get('/mostrareg', function () {
+$nom="Jaume";
+$dades=Treballador::where("nom","=",$nom)->first();
+echo $dades["cognoms"]."\n";
+});
+Route::delete('/delreg', function () {
+$nom="Jaume";
+Treballador::where("nom","=",$nom)->delete();
 });
